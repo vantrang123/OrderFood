6458d7d14d4bd9d64aee14ad50db075d009cbf1e
arens2 <baolkschool@gmail.com>
2019-10-14 17:56:40 +0700

    setScrollBar for Home()

--- app/src/main/java/com/trangdv/orderfood/ui/MainActivity.java
+++ app/src/main/java/com/trangdv/orderfood/ui/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void Home() {
-        //setScrollBar(1);
+        //setScrollBar(1); 
         setScrollBar(SCROLL_FLAG_SCROLL|SCROLL_FLAG_ENTER_ALWAYS);
         fragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, new HomeFragment())
